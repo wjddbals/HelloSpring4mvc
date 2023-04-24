098cf78cff017f6b87a9b47a749910cc2ed96fb7
FULLSTACK <jumin0220@naver.com>
2023-04-24 18:21:53 +0900

    저장소초기화

--- src/main/java/jiho/spring4/mvc/controller/HomeController.java
+++ src/main/java/jiho/spring4/mvc/controller/HomeController.java
@@ -1,12 +1,15 @@
 package jiho.spring4.mvc.controller;
 
-import javax.xml.ws.RequestWrapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value="/",nmethod=RequsetMethod.GET)
-    public String home(){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+
         return "home";
     }
 
